1de94297399f1f5dc7524dbe198121a60ba955e2
abdrahman22053 <abderahmanhaimeda@gmail.com>
2026-06-19 00:42:34 +0000

    change

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# 1. Installer Tomcat 10
 sudo dnf install -y tomcat10
 
+# 2. Forcer la création du dossier s'il n'existe pas
 sudo mkdir -p /var/lib/tomcat10/webapps/
 
-sudo chmod 777 /var/lib/tomcat10/webapps/
+# 3. NETTOYAGE : Supprimer l'ancien fichier .war et son dossier déployé avec les droits root
+sudo rm -rf /var/lib/tomcat10/webapps/lab2-webapp.war
+sudo rm -rf /var/lib/tomcat10/webapps/lab2-webapp/
+
+# 4. Donner la propriété du dossier à tomcat mais laisser les droits d'écriture
+sudo chown -R tomcat:tomcat /var/lib/tomcat10/webapps/
+sudo chmod -R 777 /var/lib/tomcat10/webapps/
